9da6dc4dac87a865a89f2e3aac2922f0554da430
Dianis Caro <dm.caro@uniandes.edu.co>
2019-09-11 10:15:58 -0500

    Admin Logic

--- s4_dispositivos-back/src/main/java/co/edu/uniandes/csw/dispositivos/ejb/AdministradorLogic.java
+++ s4_dispositivos-back/src/main/java/co/edu/uniandes/csw/dispositivos/ejb/AdministradorLogic.java
@@ -8,6 +8,7 @@ package co.edu.uniandes.csw.dispositivos.ejb;
 import co.edu.uniandes.csw.dispositivos.entities.AdministradorEntity;
 import co.edu.uniandes.csw.dispositivos.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.dispositivos.persistence.AdministradorPersistence;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -22,23 +23,60 @@ public class AdministradorLogic
  private AdministradorPersistence persistence;
  
     /**
-     *
-     * @param admin
-     * @return
-     * @throws BusinessLogicException
+     *Se encarga de crear un administrador en la base de datos
+     * @param admin Objeto de AdministadorEntity con los datos nuevos
+     * @return Objeto de AmdinistorEntity con los datos nuevos y su ID
+     * @throws BusinessLogicException Cuando se cumple una de las reglas de negocio
+     *         1. Usuario es nulo
+     *         2. La contraseña es nula
+     *         3. El correo suministrado no termina en @wireless.com
+     *         4. El correo es nulo
+     *         5. El correo no ha sido registrado previamente
+     *         6. El usuario no existe previamente
      */
     public AdministradorEntity createAdministrador(AdministradorEntity admin) throws BusinessLogicException
- {
+    {
      if(admin.getUsuario()==null)
          throw new BusinessLogicException("El usuario suministrado es nulo");
      if(admin.getContrasena()==null)
          throw new BusinessLogicException("La constraseña suministrada es nula");
-     if(admin.getCorreo().endsWith("@wireless.com"))
+     if(admin.getCorreo()==null)
+         throw new BusinessLogicException("El correo suministrado es nulo");
+     if(!admin.getCorreo().endsWith("@wireless.com"))
          throw new BusinessLogicException("El correo ingresado no es corporativo");
+     if (persistence.findByUser(admin.getUsuario()) != null)
+            throw new BusinessLogicException("Ya existe un Administrador con el usuario \"" + admin.getUsuario()+ "\"");
+     if (persistence.findByEmail(admin.getCorreo()) != null)
+            throw new BusinessLogicException("Ya existe un Administrador con el correo \"" + admin.getCorreo()+ "\"");
      else
      {
         admin = persistence.create(admin);
         return admin;
      }
- }
+    }
+    /**
+     * Obtiene la lista de los registros de Administrador
+     * @return Colección de objetos de AdministradorEntity
+     */
+    public List<AdministradorEntity> getAdministradores() 
+    {
+        List<AdministradorEntity> lista = persistence.findAll();
+        return lista;
+    }
+    /**
+     * Obtiene los datos de una instancia de Administrador a partir de su ID
+     * @param adminId Identificador de la instancia a consultar
+     * @return Instancia de Administrador con los datos del Administrador Id consultado.
+     * @throws BusinessLogicException Cuando se cumple una de las reglas de neocio
+     *         1. EL id proporcionado no existe
+     */
+    public AdministradorEntity getAdministrador(Long adminId) throws BusinessLogicException
+    {
+        AdministradorEntity admin = persistence.find(adminId);
+        if (admin == null) 
+        {
+            
+        }
+        return admin;
+    }
 }
---
+++ s4_dispositivos-back/src/test/java/co/edu/uniandes/csw/dispositivos/test/logic/AdministradorLogicTest.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.dispositivos.test.logic;
+
+import co.edu.uniandes.csw.dispositivos.ejb.AdministradorLogic;
+import co.edu.uniandes.csw.dispositivos.entities.AdministradorEntity;
+import co.edu.uniandes.csw.dispositivos.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.dispositivos.persistence.AdministradorPersistence;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ * Test de lógica de la clase AdminstradorLogic
+ * @author Dianis Caro
+ */
+@RunWith(Arquillian.class)
+public class AdministradorLogicTest 
+{
+    private PodamFactory factory = new PodamFactoryImpl();
+    
+    @PersistenceContext(unitName = "dispositivosPU")
+    private EntityManager em;
+    
+    @Inject 
+    private AdministradorLogic adminLogic;
+    /**
+     * Construye el despliegue de la prueba a realizar
+     * @return jar, es decir JavaArchive.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(AdministradorEntity.class.getPackage())
+                .addPackage(AdministradorPersistence.class.getPackage())
+                .addPackage(AdministradorLogic.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    /**
+     * Test para crear un administrador con todas las reglas de negocio
+     * @throws BusinessLogicException si una regla de negocio no se cumple
+     */
+    @Test
+    public  void createAdministrador() throws BusinessLogicException
+    {
+        AdministradorEntity adminEntity = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity.setCorreo("correoW@wireless.com");
+        AdministradorEntity result = adminLogic.createAdministrador(adminEntity);
+        Assert.assertNotNull(result);
+        
+        AdministradorEntity entity = em.find(AdministradorEntity.class, result.getId());
+        Assert.assertEquals(entity.getUsuario(), result.getUsuario());
+        Assert.assertEquals(entity.getContrasena(), result.getContrasena());
+        Assert.assertEquals(entity.getCorreo(), result.getCorreo());
+
+    }
+    
+    /**
+     * Test para crear un administrador sin usuario
+     * @throws BusinessLogicException si una regla de negocio no se cumple
+     */
+    @Test(expected = BusinessLogicException.class)
+    public  void createAdministradorUsuarioNull() throws BusinessLogicException
+    {
+        AdministradorEntity adminEntity = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity.setUsuario(null);
+        adminLogic.createAdministrador(adminEntity);
+    }
+    /**
+     * Test para crear un administrador sin contrseña
+     * @throws BusinessLogicException si una regla de negocio no se cumple
+     */
+    @Test(expected = BusinessLogicException.class)
+    public  void createAdministradorContrasenaNull() throws BusinessLogicException
+    {
+        AdministradorEntity adminEntity = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity.setContrasena(null);
+        adminLogic.createAdministrador(adminEntity);
+    }
+    /**
+     * Test para crear un administrador sin correo
+     * @throws BusinessLogicException si una regla de negocio no se cumple
+     */
+    @Test(expected = BusinessLogicException.class)
+    public  void createAdministradorCorreoNull() throws BusinessLogicException
+    {
+        AdministradorEntity adminEntity = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity.setCorreo(null);
+        adminLogic.createAdministrador(adminEntity);
+    }
+    /**
+     * Test para crear un administrador sin correo corporativo
+     * @throws BusinessLogicException si una regla de negocio no se cumple
+     */
+    @Test(expected = BusinessLogicException.class)
+    public  void createAdministradorCorreoNoCorp() throws BusinessLogicException
+    {
+        AdministradorEntity adminEntity = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity.setCorreo("correo@uniandes.edu.co");
+        adminLogic.createAdministrador(adminEntity);
+    }
+    /**
+     * Test para crear un administrador con correo corporativo existente
+     * @throws BusinessLogicException si una regla de negocio no se cumple
+     */
+    @Test(expected = BusinessLogicException.class)
+    public  void createAdministradorCorreoExistente() throws BusinessLogicException
+    {
+        AdministradorEntity adminEntity = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity.setCorreo("correo@wireless.com");
+        AdministradorEntity result = adminLogic.createAdministrador(adminEntity);
+        Assert.assertNotNull(result);
+        
+        AdministradorEntity adminEntity2 = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity2.setCorreo("correo@wireless.com");
+        adminLogic.createAdministrador(adminEntity2);
+    }
+     /**
+     * Test para crear un administrador con usuario existente
+     * @throws BusinessLogicException si una regla de negocio no se cumple
+     */
+    @Test(expected = BusinessLogicException.class)
+    public  void createAdministradorUsuarioExistente() throws BusinessLogicException
+    {
+        AdministradorEntity adminEntity = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity.setCorreo("correo1@wireless.com");
+        adminEntity.setUsuario("admin");
+        AdministradorEntity result = adminLogic.createAdministrador(adminEntity);
+        Assert.assertNotNull(result);
+        
+        AdministradorEntity adminEntity2 = factory.manufacturePojo(AdministradorEntity.class);
+        adminEntity2.setCorreo("correo2@wireless.com");
+        adminEntity2.setUsuario("admin");
+        adminLogic.createAdministrador(adminEntity2);
+    }
+}
