5dc053253740799c77149a56d1a099088894d69e
Santiago <s.fajardot@uniandes.edu.co>
2019-09-16 21:50:51 -0500

    Solo esta creado el servicio POST

--- s4_dispositivos-api/src/main/java/co/edu/uniandes/csw/dispositivos/resources/DispositivoResource.java
+++ s4_dispositivos-api/src/main/java/co/edu/uniandes/csw/dispositivos/resources/DispositivoResource.java
@@ -6,10 +6,15 @@
 package co.edu.uniandes.csw.dispositivos.resources;
 
 import co.edu.uniandes.csw.dispositivos.dtos.DispositivoDTO;
+import co.edu.uniandes.csw.dispositivos.ejb.DispositivoLogic;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -25,9 +30,42 @@ public class DispositivoResource {
 
     private static final Logger LOGGER = Logger.getLogger(DispositivoResource.class.getName());
 
+    /**
+     *
+     * @param dispositivo
+     * @return
+     */
     @POST
     public DispositivoDTO createDispositivo(DispositivoDTO dispositivo) {
         return dispositivo;
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @GET
+    public DispositivoDTO getDispositivo(Long id) {
+        return null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @PUT
+    public DispositivoDTO updateDispositivo() {
+        return null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @DELETE
+    public DispositivoDTO deleteDispotivo() {
+        return null;
     }
 
 }
