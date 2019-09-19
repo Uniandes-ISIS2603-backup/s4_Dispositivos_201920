eeb8f3a723502c4e14e640852a3599fd56046cf8
Dianis Caro <dm.caro@uniandes.edu.co>
2019-09-19 18:06:57 -0500

    DTOs

--- s4_dispositivos-api/src/main/java/co/edu/uniandes/csw/dispositivos/resources/AdministradorResource.java
+++ s4_dispositivos-api/src/main/java/co/edu/uniandes/csw/dispositivos/resources/AdministradorResource.java
@@ -6,13 +6,9 @@
 package co.edu.uniandes.csw.dispositivos.resources;
 
 import co.edu.uniandes.csw.dispositivos.dtos.AdministradorDTO;
-import co.edu.uniandes.csw.dispositivos.ejb.AdministradorLogic;
-import co.edu.uniandes.csw.dispositivos.entities.AdministradorEntity;
 import co.edu.uniandes.csw.dispositivos.exceptions.BusinessLogicException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -21,7 +17,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 
 /**
  * Clase que implementa el recurso "administrador"
@@ -32,63 +27,33 @@ import javax.ws.rs.WebApplicationException;
 @Consumes("application/json")
 @RequestScoped
 public class AdministradorResource 
-{
-    @Inject
-    private AdministradorLogic adminLogic;
-    
+{    
     @POST
     public AdministradorDTO createAdministrador(AdministradorDTO administrador) throws BusinessLogicException {
-        AdministradorDTO nuevoAdminDTO = new AdministradorDTO(adminLogic.createAdministrador(administrador.toEntity()));
-        return nuevoAdminDTO;
+        return null;
     }
         
     @GET
     @Path("{adminId: \\d+}")
     public AdministradorDTO getAdministrador(@PathParam("adminId") Long adminId) throws BusinessLogicException
     {
-        AdministradorEntity entity = adminLogic.getAdministrador(adminId);
-        if (entity == null) {
-            throw new WebApplicationException("El recurso /administradores/" + adminId + " no existe.", 404);
-        }
-        AdministradorDTO adminDTO = new AdministradorDTO(entity);
-        return adminDTO;
+        return null;
     }
     
     @PUT
     @Path("{adminId: \\d+}")
     public AdministradorDTO updateBook(@PathParam("adminId") Long adminId, AdministradorDTO admin) throws BusinessLogicException {
-        admin.setId(adminId);
-        if (adminLogic.getAdministrador(adminId) == null) 
-        {
-            throw new WebApplicationException("El recurso /administradores/" + adminId + " no existe.", 404);
-        }
-        AdministradorDTO adminDTO = new AdministradorDTO(adminLogic.updateAdministrador(adminId, admin.toEntity()));
-        return adminDTO;
+        return null;
     }
     
     @DELETE
     @Path("{adminId: \\d+}")
     public void deleteBook(@PathParam("adminId") Long adminId) throws BusinessLogicException {
-        AdministradorEntity entity = adminLogic.getAdministrador(adminId);
-        if (entity == null) {
-            throw new WebApplicationException("El recurso /administradores/" + adminId + " no existe.", 404);
-        }
-        adminLogic.deleteAdministrador(adminId);
     }
     
     @GET
     public List<AdministradorDTO> getBooks() {
-        List<AdministradorDTO> listaAdmins = listEntity2DetailDTO(adminLogic.getAdministradores());
-        return listaAdmins;
-    }
-    
-    private List<AdministradorDTO> listEntity2DetailDTO(List<AdministradorEntity> entityList) {
-        List<AdministradorDTO> list = new ArrayList<>();
-        for (AdministradorEntity entity : entityList) 
-        {
-            list.add(new AdministradorDTO(entity));
-        }
-        return list;
+        return null;
     }
     
 }
--- s4_dispositivos-api/src/main/java/co/edu/uniandes/csw/dispositivos/resources/ComprobanteDePagoResource.java
+++ s4_dispositivos-api/src/main/java/co/edu/uniandes/csw/dispositivos/resources/ComprobanteDePagoResource.java
@@ -6,21 +6,16 @@
 package co.edu.uniandes.csw.dispositivos.resources;
 
 import co.edu.uniandes.csw.dispositivos.dtos.ComprobanteDePagoDTO;
-import co.edu.uniandes.csw.dispositivos.ejb.ComprobanteDePagoLogic;
-import co.edu.uniandes.csw.dispositivos.exceptions.BusinessLogicException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import co.edu.uniandes.csw.dispositivos.entities.ComprobanteDePagoEntity;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.PUT;
 
 /**
  * Clase que implementa el recurso "comprobanteDePago"
@@ -31,16 +26,32 @@ import javax.ws.rs.WebApplicationException;
 @Consumes("application/json")
 @RequestScoped
 public class ComprobanteDePagoResource 
-{
-    @Inject
-    private ComprobanteDePagoLogic comprobanteLogic;
-    
-    private List<ComprobanteDePagoDTO> listEntity2DTO(List<ComprobanteDePagoEntity> entityList) {
-        List<ComprobanteDePagoDTO> list = new ArrayList<ComprobanteDePagoDTO>();
-        for (ComprobanteDePagoEntity entity : entityList) 
-        {
-            list.add(new ComprobanteDePagoDTO(entity));
-        }
-        return list;
+{    
+ @POST
+    public ComprobanteDePagoDTO createReview(@PathParam("clienteId") Long clienteId, ComprobanteDePagoDTO comprobante){
+        return null;
+    }
+
+    @GET
+    public List<ComprobanteDePagoDTO> getReviews(@PathParam("clienteId") Long clienteId) {
+        return null;
+    }
+
+    @GET
+    @Path("{comprobanteId: \\d+}")
+    public ComprobanteDePagoDTO getReview(@PathParam("clienteId") Long clienteId, @PathParam("comprobanteId") Long comprobanteId) {
+        return null;
+    }
+
+    @PUT
+    @Path("{comprobanteId: \\d+}")
+    public ComprobanteDePagoDTO updateReview(@PathParam("clienteId") Long clienteId, @PathParam("comprobanteId") Long reviewsId, ComprobanteDePagoDTO comprobante) {
+        return null;
+    }
+
+    @DELETE
+    @Path("{comprobanteId: \\d+}")
+    public void deleteReview(@PathParam("clienteId") Long clienteId, @PathParam("comprobanteId") Long comprobanteId){
+        
     }
 }
