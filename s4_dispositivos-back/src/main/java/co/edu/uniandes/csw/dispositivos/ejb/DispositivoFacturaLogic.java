6cd984acc878b7f5505531eace3c7befe1c80052
Santiago <s.fajardot@uniandes.edu.co>
2019-09-23 15:11:31 -0500

    TestPersistence Dispositivo listo con todas las asociaciones

--- s4_dispositivos-back/src/main/java/co/edu/uniandes/csw/dispositivos/ejb/DispositivoFacturaLogic.java
+++ s4_dispositivos-back/src/main/java/co/edu/uniandes/csw/dispositivos/ejb/DispositivoFacturaLogic.java
@@ -5,7 +5,13 @@
  */
 package co.edu.uniandes.csw.dispositivos.ejb;
 
+import co.edu.uniandes.csw.dispositivos.entities.DispositivoEntity;
+import co.edu.uniandes.csw.dispositivos.entities.FacturaEntity;
+import co.edu.uniandes.csw.dispositivos.persistence.DispositivoPersistence;
+import co.edu.uniandes.csw.dispositivos.persistence.FacturaPersistence;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 /**
  *
@@ -13,5 +19,34 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class DispositivoFacturaLogic {
+
+    private static final Logger LOGGER = Logger.getLogger(DispositivoFacturaLogic.class.getName());
+
+    @Inject
+    private DispositivoPersistence dispositivoPersistence;
+
+    @Inject
+    private FacturaPersistence facturaPersistence;
+
+    /**
+     *
+     * @param dispositivoId
+     * @param facturaId
+     * @return
+     */
+    public DispositivoEntity replaceFactura(Long dispositivoId, Long facturaId) {
+        DispositivoEntity dispositivoEntity = dispositivoPersistence.find(dispositivoId);
+        FacturaEntity facturaEntity = facturaPersistence.find(facturaId);
+        dispositivoEntity.setFactura(facturaEntity);
+        return dispositivoEntity;
+    }
+
+    public void removeFactura(Long dispositivoId) {
+
+        DispositivoEntity dispositivoEntity = dispositivoPersistence.find(dispositivoId);
+        FacturaEntity facturaEntity = facturaPersistence.find(dispositivoEntity.getFactura().getId());
+        dispositivoEntity.setFactura(null);
+        facturaEntity.getDispositivos().remove(dispositivoEntity);
+    }
 
 }
