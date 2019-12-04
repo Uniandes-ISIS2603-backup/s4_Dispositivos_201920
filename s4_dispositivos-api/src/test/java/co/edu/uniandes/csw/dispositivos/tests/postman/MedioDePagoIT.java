af8e7bcb4543dfe7c02606bf23d8c12427062e97
Juan L <sulogin@uniandes.edu.co7>
2019-12-04 15:14:56 -0500

    Correcion de medios

--- s4_dispositivos-api/src/test/java/co/edu/uniandes/csw/dispositivos/tests/postman/MedioDePagoIT.java
+++ s4_dispositivos-api/src/test/java/co/edu/uniandes/csw/dispositivos/tests/postman/MedioDePagoIT.java
@@ -31,6 +31,7 @@ public class MedioDePagoIT {
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class, "s4_dispositivos-api.war")//War del modulo api
                 // Se agrega las dependencias
+                // Se agrega las dependencias
                 .addAsLibraries(Maven.resolver().loadPomFromFile("pom.xml")
                         .importRuntimeDependencies().resolve()
                         .withTransitivity().asFile())
@@ -51,7 +52,7 @@ public class MedioDePagoIT {
     @RunAsClient
     public void postman() throws IOException {
         PostmanTestBuilder tp = new PostmanTestBuilder();
-        tp.setTestWithoutLogin(COLLECTION, "Entorno-IT.postman_environment");
+        tp.setTestWithoutLogin(COLLECTION, "Entorno-Colecciones.postman_environment");
         String desiredResult = "0";
         Assert.assertEquals("Error en Iterations de: " + COLLECTION, desiredResult, tp.getIterations_failed());
 
